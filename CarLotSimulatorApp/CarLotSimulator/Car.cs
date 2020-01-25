07fd151e390629f9a65eb9659dcdaf2cd661e568
Jack in the Boxx <kdijahsweets@gmail.com>
2020-01-25 16:55:45 -0600

    First Attempt at Classes

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Car
+{
+    public Car()
+    {
+
+    }
+    // Practice writing methods and classes without adding semi-colons!!
+
+    public int CarYear { get; set; }
+    public string CarMake { get; set; }
+    public string CarModel { get; set; }
+    public string EngineNoise { get; set; }
+    public string BeeperHorn { get; set; }
+    public bool IsDriveable { get; set; }
+
+    public void EngineSound(string engineNoise)
+    {
+        EngineNoise = engineNoise;
+    }
+
+    public void HornNoise(string beeperHorn) 
+    {
+        HornNoise = beeperHorn;
+        
+    }
+
+
+}
+
+
+
+
+
+
+
